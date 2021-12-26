2b1082d4143a63603bf0637bf5b7edc1a8647c11
RangerVinven <daniel.mcpherson@live.co.uk>
2021-12-26 21:07:10 +0000

    Added the axios package to call the API

--- notes-frontend/src/App.tsx
+++ notes-frontend/src/App.tsx
@@ -3,22 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const getNotes = () => {
+    console.log("Hello World!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Notes Application</div>
+      <div>
+        <button onClick={getNotes}>Click Me</button>
+      </div>
     </div>
   );
 }
